dc67c1014ee94e634f6176290ff565ae7604abd2
InYaaahead <LiesZharaMedia@hotmail.com>
2025-12-23 13:16:25 +0100

    Update config.js

--- config.js
+++ config.js
@@ -30,9 +30,9 @@ class EnterpriseConfigurationManager {
              * Configure your Lavalink server for audio processing
              */
             lavalink: {
-                host: EnvironmentVariableProcessor.LAVALINK_HOST || "ca.lavalink.yumistack.net", 
-                port: EnvironmentVariableProcessor.LAVALINK_PORT || 2333,       
-                password: EnvironmentVariableProcessor.LAVALINK_PASSWORD || "Sakura", 
+                host: EnvironmentVariableProcessor.LAVALINK_HOST || "217.160.125.127", 
+                port: EnvironmentVariableProcessor.LAVALINK_PORT || 14845,       
+                password: EnvironmentVariableProcessor.LAVALINK_PASSWORD || "prmgvyt", 
                 secure: EnvironmentVariableProcessor.LAVALINK_SECURE === 'false' || true
             },
             
@@ -101,6 +101,7 @@ module.exports = primaryApplicationConfiguration;
  * Never share your bot token or database URI publicly!
  * Use environment variables in production!
  */
+
 
 
 
